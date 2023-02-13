e8cd9d35c00e1e0f8ca4ae785254b1f8f3ce91bd
viniciussluiz <103579574+viniciussluiz@users.noreply.github.com>
2023-02-13 10:14:58 -0300

    exercícios feitos

---
+++ pythonEstudos/aula013.py
@@ -0,0 +1,12 @@
+# Estrutura de repetição - for
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+
+for numero in range (1, 6):
+    print(numero)
+
+for numero in range (5, 0, -1):
+    print(numero)
