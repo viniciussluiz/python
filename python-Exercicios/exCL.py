dca5b7ca3dd582f29f369dcea59eb4951998a069
viniciussluiz <103579574+viniciussluiz@users.noreply.github.com>
2023-03-16 13:44:58 -0300

    exercicio quartas champions criado

---
+++ python-Exercicios/exCL.py
@@ -0,0 +1,7 @@
+import random
+times = ['Napoli', 'Real Madrid', 'Chelsea', 'Manchester City', 'Milan', 'Inter de Milão', 'Bayern', 'Benfica']
+sorteio = random.shuffle(times)
+print(times)
+print(f'{times[0]} x {times[1]}\n{times[2]} x {times[3]}\n{times[4]} x {times[5]}\n{times[6]} x {times[7]}')
+
+
